fe957efc12677922be37278bbb1ea3344f7ba99c
gitul0515 <gitul0515@gmail.com>
2021-11-22 20:56:51 +0900

    공 던지기 효과 완성 & 리팩터링

--- 1_Smart_ForkGame_Tuho/main.java
+++ 1_Smart_ForkGame_Tuho/main.java
@@ -27,8 +27,9 @@ class Stage {
 }
 
 int cur_stage = 1; // 현재 스테이지
-boolean answer_status = false; // 정답 판정
-int wrong_status = 0; // 오답 판정
+boolean answer_status = false; // 정답을 맞추었는지 판정하는 변수
+int ring_number = 0; // 플레이어가 답을 고른 링 번호
+int wrong_number = 0; // 오답인 경우 오답 번호
 
 // 애니메이션
 PImage[] img_time = new PImage[16]; // 시간 애니메이션
@@ -44,7 +45,6 @@ int gameOver_idx = 0;
 
 // 공
 int ball_diam = 330; // 공의 지름
-boolean ball_effect = false; // 공 던지기 효과 (false: off, true: on)
 
 void setup() {
   // 화면 크기 (모니터 해상도)
@@ -74,7 +74,7 @@ void setup() {
     img_stars[i] = loadImage("stars-" + i + ".png");
   }
   
-  // 정답1 애니메이션
+  // 정답 애니메이션
   for (int i = 0; i < img_answer_1.length; i++) {
     img_answer_1[i] = loadImage("answer-1-" + i + ".png");
   }
@@ -94,7 +94,7 @@ void draw() {
   // 배경색 (옅은 회색)
   background(242, 242, 242);
 
-  // 시간(time)은 1초 단위로 차감된다.
+  // 시간은 1초 단위로 차감된다.
   int time = 15 - (millis() / 1000 - reset_time); // reset_time은 시간 초기화용 변수
   
   // 시간(15초)가 지나지 않은 경우
@@ -102,27 +102,27 @@ void draw() {
     // 첫 번째 스테이지
     if (cur_stage == 1) {
       if (answer_status == false) {
-        quiz(time, stage[cur_stage - 1]);
-      } else Answer(stage[cur_stage - 1]);
+        quizScreen(time, stage[cur_stage - 1]); // 퀴즈 화면을 출력한다
+      } else answerScreen(stage[cur_stage - 1]); // 정답 화면을 출력한다
     }
         
     // 두 번째 스테이지
     if (cur_stage == 2) {
       if (answer_status == false) {
-        quiz(time, stage[cur_stage - 1]);
-      } else Answer(stage[cur_stage - 1]);
+        quizScreen(time, stage[cur_stage - 1]);
+      } else answerScreen(stage[cur_stage - 1]);
     } 
     
     // 세 번째 스테이지
     if (cur_stage == 3) {
       if (answer_status == false) {
-        quiz(time, stage[cur_stage - 1]);
-      } else Answer(stage[cur_stage - 1]);
+        quizScreen(time, stage[cur_stage - 1]);
+      } else answerScreen(stage[cur_stage - 1]);
     }
     
     // 게임 클리어
     if (cur_stage > 3) {
-      gameClear();
+      gameClear(); // 게임 클리어 화면을 출력한다
     }
   } else { // 시간(15초)가 지났다면, 게임 오버 화면을 출력한다
     gameOver();
@@ -130,7 +130,7 @@ void draw() {
 }
 
 // 퀴즈 화면을 출력하는 함수
-void quiz(int time, Stage stage) {
+void quizScreen(int time, Stage stage) {
   textAlign(CENTER);
   
   // 문제 텍스트
@@ -192,77 +192,60 @@ void quiz(int time, Stage stage) {
     opacity_var *= -1;
   }
     
-  // ============= 정답 및 오답 처리 =============
-  // 정답이 1인 경우
-  if (stage.answer == 1 && mousePressed && mouseY >= 305 && mouseY <= 575) {
-    if (mouseX >= 205 && mouseX <= 475) { // 정답 처리
-      ball_effect = true; // 공 던지기 효과
-    } else if (mouseX >= 555 && mouseX <= 825) { // 오답 처리: 2
-      wrong_status = 2;
-    } else if (mouseX >= 905 && mouseX <= 1175) { // 오답 처리: 3
-      wrong_status = 3;
+  // 링에 공을 넣은 경우
+  if (mousePressed && mouseY >= 305 && mouseY <= 575) {
+    if (mouseX >= 205 && mouseX <= 475) { 
+      ring_number = 1; // 1번 링에 공 삽입
+    } else if (mouseX >= 555 && mouseX <= 825) {
+      ring_number = 2; // 2번 링에 공 삽입
+    } else if (mouseX >= 905 && mouseX <= 1175) {
+      ring_number = 3; // 3번 링에 공 삽입
     }
   }
   
-  // 정답이 2인 경우
-  if (stage.answer == 2 && mousePressed && mouseY >= 305 && mouseY <= 575) {
-    // 정답 처리
-    if (mouseX >= 555 && mouseX <= 825) {
-      ball_effect = true; // 공 던지기 효과
-    } else if (mouseX >= 205 && mouseX <= 475) { // 오답 처리: 1
-      wrong_status = 1;
-    } else if (mouseX >= 905 && mouseX <= 1175) { // 오답 처리: 3
-      wrong_status = 3;
-    }
+  // 답을 판정한다
+  if (boolean(ring_number)) { // ring_number가 0이면 실행하지 않음. 1, 2, 3이면 실행함
+    judgeAnswer(stage);
   }
   
-  // 정답이 3인 경우
-  if (stage.answer == 3 && mousePressed && mouseY >= 305 && mouseY <= 575) {
-    // 정답 처리
-    if (mouseX >= 905 && mouseX <= 1175) {
-      ball_effect = true; // 공 던지기 효과
-    } else if (mouseX >= 205 && mouseX <= 475) { // 오답 처리: 1
-      wrong_status = 1;
-    } else if (mouseX >= 555 && mouseX <= 825) { // 오답 처리: 2
-      wrong_status = 2;
-    }
-  }
-  
-  if (ball_effect) {
-    ball(stage); // 공 던지기 함수
-  }
-  
-  if (boolean(wrong_status)) { // wrong_status가 0이면 실행x, 1 2 3이면 실행o
-    Wrong(wrong_status);  
+  // 오답을 표시한다
+  if (boolean(wrong_number)) { // wrong_number가 0이면 실행하지 않음. 1 2 3이면 실행함
+    wrongAnswer(wrong_number);  
   }
 }
 
-// 정답 선택 시 공 던지기 효과
-void ball(Stage stage) {
+// 답을 판정하는 함수
+void judgeAnswer(Stage stage) {
+  // 링에 공을 던지는 시각 효과
   noStroke();
   fill(255, 212, 0);
-
-  if (stage.answer == 1) {
+  if (ring_number == 1) {
     ellipse(340, 440, ball_diam, ball_diam);
-  } else if (stage.answer == 2) {
+  } else if (ring_number == 2) {
     ellipse(width / 2, 440, ball_diam, ball_diam);
-  } else if (stage.answer == 3){
+  } else if (ring_number == 3){
     ellipse(1026, 440, ball_diam, ball_diam);
   }
-  ball_diam -= 10;
+  ball_diam -= 10; // 공의 크기가 점점 작아진다
   delay(10);
   
-  // 공의 크기가 작아지면 정답 화면으로 넘어간다
-  if (ball_diam <= 70) {
-    answer_status = true;
-    // 공 초기화
-    ball_diam = 330; 
-    ball_effect = false;
+  // 공의 지름이 90 미만으로 작아지면 답을 판정한다
+  if (ball_diam < 90) {
+    if (ring_number == stage.answer) { // 정답인 경우
+      answer_status = true; // 정답 화면으로 넘어간다
+    } else { // 오답인 경우: 오답 표시
+      if (ring_number == 1) wrong_number = 1;
+      else if (ring_number == 2) wrong_number = 2;
+      else if (ring_number == 3) wrong_number = 3;
+    }
+    // 변수 초기화
+    ball_diam = 330;
+    ring_number = 0;
   }
 }
 
 // 정답 화면
-void Answer(Stage stage) {
+void answerScreen(Stage stage) {
   reset_time = millis() / 1000; // 시간을 멈춘다
   
   textFont(font2, 62);
@@ -312,24 +295,24 @@ void Answer(Stage stage) {
     if (key == ' ') { // 스페이스 바를 누르면 다음 스테이지로 이동한다
       cur_stage++; // 스테이지 1 증가
       answer_status = false;
-      wrong_status = 0;
+      wrong_number = 0;
       reset_time = millis() / 1000; // 시간 초기화
     }
   }
 }
 
 // 오답을 X선으로 표시한다
-void Wrong(int wrong_status) {
+void wrongAnswer(int wrong_number) {
   strokeWeight(10);
   stroke(51, 63, 80, 150);
   
-  if (wrong_status == 1) {
+  if (wrong_number == 1) {
     line(257, 354, 427, 525);
     line(427, 354, 257, 525);
-  } else if (wrong_status == 2) {
+  } else if (wrong_number == 2) {
     line(597, 354, 767, 525);
     line(767, 354, 597, 525);
-  } else if (wrong_status == 3) {
+  } else if (wrong_number == 3) {
     line(940, 354, 1110, 525);
     line(1110, 354, 940, 525);
   }
@@ -367,7 +350,7 @@ void gameOver() {
       // 변수들을 초기화하고, 첫 번째 퀴즈로 돌아간다
       cur_stage = 1;
       answer_status = false;
-      wrong_status = 0;
+      wrong_number = 0;
       reset_time = millis() / 1000; // 시간 초기화
     }
   } 
@@ -402,7 +385,7 @@ void gameClear() {
       // 변수들을 초기화하고, 첫 번째 퀴즈로 돌아간다
       cur_stage = 1;
       answer_status = false;
-      wrong_status = 0;
+      wrong_number = 0;
       reset_time = millis() / 1000; // 시간 초기화
     }
   } 
